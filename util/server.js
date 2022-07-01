9a6ae342fc1d2213a502abf93a43333c972f75c7
wingyuen2022 <wingyuenuk330@gmail.com>
2022-07-01 03:55:21 +0100

    Merged docker git to server git

--- util/server.js
+++ util/server.js
@@ -5,11 +5,11 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
-const contentRoutes = require('./routes/content');
-const userRoutes = require('./routes/user');
-const goalRoutes = require('./routes/goal');
-const workoutRoutes = require('./routes/workout');
-const testingRoutes = require('./routes/testing');
+const contentRoutes = require('../routes/content');
+const userRoutes = require('../routes/user');
+const goalRoutes = require('../routes/goal');
+const workoutRoutes = require('../routes/workout');
+const testingRoutes = require('../routes/testing');
 server.use('/content', contentRoutes);
 server.use('/user', userRoutes);
 server.use('/goal', goalRoutes);
